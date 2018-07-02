2f420c8e6085c6fe4b00537c3d5fe8605bd57cde
Marco Montanari <sirmmo@gmail.com>
2018-07-02 14:42:04 +0200

    script

--- build_em_all.sh
+++ build_em_all.sh
@@ -1,7 +1,7 @@
 cp .npmrc.template $HOME/.npmrc
 
-ng build mn-geo && cd dist/mn-geo && npm publish && cd ../..
-ng build mn-map && cd dist/mn-map && npm publish && cd ../..
-ng build mn-mapgl && cd dist/mn-mapgl && npm publish && cd ../..
+ng build mn-geo && cd dist/mn-geo && npm publish --access public && cd ../..
+ng build mn-map && cd dist/mn-map && npm publish --access public && cd ../..
+ng build mn-mapgl && cd dist/mn-mapgl && npm publish --access public && cd ../..
 
 
