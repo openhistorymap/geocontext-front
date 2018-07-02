a4aca951e16f1607cc8a724e719b19420814cb46
Marco Montanari <sirmmo@gmail.com>
2018-07-02 14:52:57 +0200

    Bumped version to 0.1.0

--- build_em_all.sh
+++ build_em_all.sh
@@ -1,7 +1,9 @@
 cp .npmrc.template $HOME/.npmrc
 
-ng build mn-geo && cd dist/mn-geo && npm publish --access public && cd ../..
-ng build mn-map && cd dist/mn-map && npm publish --access public && cd ../..
+ng build mn-geo   && cd dist/mn-geo   && npm publish --access public && cd ../..
+ng build mn-map   && cd dist/mn-map   && npm publish --access public && cd ../..
 ng build mn-mapgl && cd dist/mn-mapgl && npm publish --access public && cd ../..
+ng build gcx-core && cd dist/gcx-core && npm publish --access public && cd ../..
+ng build ohm-core && cd dist/ohm-core && npm publish --access public && cd ../..
 
 
