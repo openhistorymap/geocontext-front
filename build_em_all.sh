25093d2a7bdad3e1d6e73c25a54ad1afad39057b
Marco Montanari <sirmmo@gmail.com>
2018-08-18 20:00:01 +0200

    correct build commands

--- build_em_all.sh
+++ build_em_all.sh
@@ -6,9 +6,9 @@ ng build mn-geo-layers-osm --prod    && cd dist/mn-layers-osm    && npm publish 
 ng build mn-geo-layers-stamen --prod && cd dist/mn-layers-stamen && npm publish --access public && cd ../..
 ng build mn-geo-datasources --prod   && cd dist/mn-datasources   && npm publish --access public && cd ../..
 ng build mn-map --prod               && cd dist/mn-map           && npm publish --access public && cd ../..
-ng build mn-mapgl --prod             && cd dist/mn-mapgl         && npm publish --access public && cd ../..
+ng build mn-mapgl --prod            #&& cd dist/mn-mapgl         && npm publish --access public && cd ../..
 ng build gcx-core --prod             && cd dist/gcx-core         && npm publish --access public && cd ../..
 ng build chcx-static --prod          && cd dist/chcx-static      && npm publish --access public && cd ../..
-ng build ohm-core --prod #&& cd dist/ohm-core && npm publish --access public && cd ../..
+ng build ohm-core --prod            #&& cd dist/ohm-core && npm publish --access public && cd ../..
 
 
