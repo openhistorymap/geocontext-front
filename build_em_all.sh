34b55a3f052a3847649cf588950640050dbcc1c4
Marco Montanari <sirmmo@gmail.com>
2018-08-19 12:36:02 +0200

    major upgrades

--- build_em_all.sh
+++ build_em_all.sh
@@ -1,14 +1,16 @@
-cp .npmrc.template $HOME/.npmrc
+#cp .npmrc.template $HOME/.npmrc
 
-ng build mn-geo --prod               && cd dist/mn-geo           && npm publish --access public && cd ../..
-ng build mn-geo-layers --prod        && cd dist/mn-layers        && npm publish --access public && cd ../..
-ng build mn-geo-layers-osm --prod    && cd dist/mn-geo-layers-osm    && npm publish --access public && cd ../..
-ng build mn-geo-layers-stamen --prod && cd dist/mn-geo-layers-stamen && npm publish --access public && cd ../..
-ng build mn-geo-datasources --prod   && cd dist/mn-datasources   && npm publish --access public && cd ../..
-ng build mn-map --prod               && cd dist/mn-map           && npm publish --access public && cd ../..
-#ng build mn-mapgl --prod             && cd dist/mn-mapgl         && npm publish --access public && cd ../..
-ng build gcx-core --prod             && cd dist/gcx-core         && npm publish --access public && cd ../..
-ng build chcx-static --prod          && cd dist/chcx-static      && npm publish --access public && cd ../..
-#ng build ohm-core --prod             && cd dist/ohm-core && npm publish --access public && cd ../..
+#npm run compodoc
+
+#ng build mn-geo                      #&& cd dist/mn-geo               && npm publish --access public && cd ../..
+#ng build mn-geo-layers --prod        && cd dist/mn-geo-layers            && npm publish --access public && cd ../..
+#ng build mn-geo-layers-osm --prod    && cd dist/mn-geo-layers-osm    && npm publish --access public && cd ../..
+#ng build mn-geo-layers-stamen --prod && cd dist/mn-geo-layers-stamen && npm publish --access public && cd ../..
+#ng build mn-geo-datasources --prod   && cd dist/mn-datasources       && npm publish --access public && cd ../..
+#ng build mn-map --prod               && cd dist/mn-map               && npm publish --access public && cd ../..
+#ng build mn-mapgl --prod             && cd dist/mn-mapgl             && npm publish --access public && cd ../..
+#ng build gcx-core --prod             && cd dist/gcx-core             && npm publish --access public && cd ../..
+#ng build chcx-static --prod          && cd dist/chcx-static          && npm publish --access public && cd ../..
+#ng build ohm-core --prod             && cd dist/ohm-core             && npm publish --access public && cd ../..
 
 
